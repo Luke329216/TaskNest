28262350e28f0432b8d314ded234a5f58e2162be
Cole254674 <cmccreary19@icloud.com>
2026-07-08 10:10:10 -0400

    gnfz

--- Dev/obj/Debug/net10.0/TaskNest.AssemblyInfo.cs
+++ Dev/obj/Debug/net10.0/TaskNest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e0d974bfbbc08e217c61eaf894c042a9887344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174db04faf9194b9a23eda419c89f0672c6c024f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net10.0/Dev.AssemblyInfo.cs
+++ obj/Debug/net10.0/Dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29508f95391dbf30ceb929448b0fe05f7df5500b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174db04faf9194b9a23eda419c89f0672c6c024f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
